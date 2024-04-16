3860aeb241b2331175ee56a4937c8dd7ef4e5388
rayan <cheklat56@gmail.com>
2024-04-16 23:50:23 +0200

    finish api

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -12,7 +12,7 @@ use App\Entity\Modules;
 
 class APIController extends AbstractController
 {
-    #[Route('/api/modules/{id}', name: 'api', methods: ['GET'])]
+    #[Route('/api/module/{id}', name: 'api_get_module', methods: ['GET'])]
     public function index(EntityManagerInterface $entityManager, $id): JsonResponse
     {
         $module = $entityManager->getRepository(Modules::class)->find($id);   
@@ -21,7 +21,7 @@ class APIController extends AbstractController
         return $this->json($data);
     }
 
-    #[Route('/api/modules/panne', name: 'api', methods: ['GET'])]
+    #[Route('/api/modules/panne', name: 'api_get_pannes', methods: ['GET'])]
     public function panne(EntityManagerInterface $entityManager): JsonResponse
     {
         $modules = $entityManager->getRepository(Modules::class)->findAll();
@@ -36,17 +36,39 @@ class APIController extends AbstractController
         return $this->json($data);
     }
 
-    #[Route('/api/genererPannes', name: 'api', methods: ['POST'])]
-    public function generer_pannes(EntityManagerInterface $entityManager): JsonResponse
+    #[Route('/api/updatePannes', name: 'api_update_pannes', methods: ['POST'])]
+    public function update_pannes(EntityManagerInterface $entityManager): JsonResponse
     {
-     
+        $modules = $entityManager->getRepository(Modules::class)->findAll();
+        $etat_possible = ["Fonctionnel", "En maintenance", "En panne"];
+        $data = [];
+
+        foreach ($modules as $key => $module) {
+            $new_etat = $etat_possible[rand(0, count($etat_possible)-1)];
+            $module->setEtat($new_etat);
+            $entityManager->persist($module);
+            $entityManager->flush();
+            array_push($data, [$module->getNom()." (".$module->getNumeroSerie().")" => $new_etat]);
+        }
         return $this->json($data);
     }
 
-    #[Route('/api/genererDonnees', name: 'api', methods: ['POST'])]
+    #[Route('/api/genererDonnees', name: 'api_generer_donnees', methods: ['POST'])]
     public function generer_donnees(EntityManagerInterface $entityManager): JsonResponse
     {
- 
-        return $this->json($data);
+        $modules = $entityManager->getRepository(Modules::class)->findAll();
+        $return = ["message" => "les données ont été envoyer avec succée"];
+        foreach ($modules as $key => $module) {
+            if ($module->getEtat() == "Fonctionnel") {
+                $date_actuelle = new \DateTime;
+                $new_data[$date_actuelle->format('Y-m-d H:i:s')] = rand(5, 50);
+                $data = $module->getDonnees();
+                array_push($data, $new_data);
+                $module->setDonnees($data);
+                $entityManager->persist($module);
+                $entityManager->flush();
+            }
+        }
+        return $this->json($return);
     }
 }
